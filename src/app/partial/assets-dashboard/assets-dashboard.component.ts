e43a2b2d3f0f655d8cf993845fb5711ec48cb808
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-10-06 18:55:53 +0530

    Item inward tab designed in assets dashboard

--- src/app/partial/assets-dashboard/assets-dashboard.component.ts
+++ src/app/partial/assets-dashboard/assets-dashboard.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+];
+
 @Component({
   selector: 'app-assets-dashboard',
   templateUrl: './assets-dashboard.component.html',
   styleUrls: ['./assets-dashboard.component.css']
 })
 export class AssetsDashboardComponent implements OnInit {
-
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/partial/assets-dashboard/assets-dashboard.module.ts
+++ src/app/partial/assets-dashboard/assets-dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AssetsDashboardRoutingModule } from './assets-dashboard-routing.module';
 import { AssetsDashboardComponent } from './assets-dashboard.component';
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AssetsDashboardComponent } from './assets-dashboard.component';
   ],
   imports: [
     CommonModule,
-    AssetsDashboardRoutingModule
+    AssetsDashboardRoutingModule,
+    MaterialModule
   ]
 })
 export class AssetsDashboardModule { }
