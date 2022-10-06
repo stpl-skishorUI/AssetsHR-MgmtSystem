3d661eb6cef1536c40fd784f3389c058a36accbe
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-10-06 14:21:32 +0530

    menu items added in sidebar

--- src/app/partial/partial-layout/sidebar/sidebar.service.ts
+++ src/app/partial/partial-layout/sidebar/sidebar.service.ts
@@ -8,162 +8,72 @@ export class SidebarService {
   _hasBackgroundImage = true;
   menus = [
     {
-      title: 'general',
+      title: 'assets',
       type: 'header'
     },
     {
-      title: 'Masters',
-      icon: 'fa fa-tachometer-alt',
+      title: 'Dashboard',
+      url:'dashboard',
+      icon: 'fa-solid fa-chart-line',
+      type: 'simple'
+    },
+    {
+      title: 'Item Master',
+      icon: 'fa-solid fa-layer-group',
       active: false,
       type: 'dropdown',
       submenus: [
         {
-          title: 'Organization Registration',
+          title: 'Group Master',
           url:'organization-registration'
         },
         {
-          title: 'Company Registration',
+          title: 'Category Master',
           url:'company-registration'
         },
         {
-          title: 'Department Registration',
+          title: 'Parameter Master',
           url:'department-registration'
-        },
-        {
-          title: 'Designation Registration',
-          url:'designation-registration'
-        },
-        {
-          title: 'Holiday Master',
-          url:'holiday-master'
-        },
-        {
-          title: 'Leave Type Registration',
-          url:'leave-type-registration'
-        },
-        {
-          title: 'Salary Type Registration',
-          url:'salary-type-registration'
-        },
-        {
-          title: 'Bank Registration',
-          url:'bank-registration'
-        },
-        {
-          title: 'Bank Branch Registration',
-          url:'bank-branch-registration'
-        },
-        {
-          title: 'Company Bank Account Registration',
-          url:'company-bank-registration'
-        },
-        {
-          title: 'Document Type Registration',
-          url:'document-type-registration'
         }
       ]
     },
     {
-      title: 'Register',
-      icon: 'fa fa-shopping-cart',
+      title: 'Store Master',
+      url:'dashboard',
+      icon: 'fa-solid fa-store',
+      type: 'simple'
+    },
+    {
+      title: 'Vendor Master',
+      url:'dashboard',
+      icon: 'fa-solid fa-screwdriver-wrench',
+      type: 'simple'
+    },
+    {
+      title: 'Items Entry',
+      icon: 'fa-solid fa-book-open',
       active: false,
       type: 'dropdown',
       submenus: [
         {
-          title: 'Employee Registration',
+          title: 'Items Registration',
           url:'employee-registration'
         },
         {
-          title: 'Employee Salary Details',
+          title: 'Item Issue/Return',
           url:'employee-salary-details'
         },
         {
-          title: 'Assign Reporting Person',
+          title: 'Item Transfer',
           url:'assign-reporting-person'
         },
         {
-          title: 'Leave Assignment',
+          title: 'Item Repair Issue/Return',
           url:'leave-assignment'
-        }
-      ]
-    },
-    {
-      title: 'HR/Admin',
-      icon: 'fa-solid fa-user',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'Absent/Compensate',
-          url:'absent-compensate'
         },
         {
-          title: 'Leave Details',
-          url:'leave-details'
-        },
-        {
-          title: 'Attendance Verification',
-          url:'attendance-verification'
-        },
-        {
-          title: 'Offer Letter',
-          url:'offer-letter'
-        },
-        {
-          title: 'Exit Form',
-          url:'exit-form'
-        }
-      ]
-    },
-    {
-      title: 'Accountant',
-      icon: 'fa-solid fa-file-invoice',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'Advance Payment',
-          url:'advance-payment'
-        },
-        {
-          title: 'Salary Deduction',
-          url:'salary-deduction'
-        },
-        {
-          title: 'Salary Processing',
-          url:'salary-processing'
-        },
-        {
-          title: 'Salary Transfer',
-          url:'salary-transfer'
-        }
-      ]
-    },
-    {
-      title: 'Report',
-      icon: 'fa-solid fa-file-lines',
-      active: false,
-      type: 'dropdown',
-      submenus: [
-        {
-          title: 'Employee Salary Report',
-          url:'employee-salary-report'
-        },
-        {
-          title: 'Salary Slip',
-          url:'salary-slip'
-        },
-        {
-          title: 'Task Details Report',
-          url:'task-details-report'
-        },
-        {
-          title: 'Leave Report',
-          url:'leave-report'
-        },
-        {
-          title: 'Project Cost',
-          url:'project-cost'
+          title: 'Item Scrap/Donated',
+          url:'leave-assignment'
         }
       ]
     },
