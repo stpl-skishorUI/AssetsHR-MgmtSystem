121f74745a8b6f7aa43603e1fa634aca42656359
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-10-12 16:53:03 +0530

    changed path for asset dashboard

--- src/app/partial/partial-layout/partial-layout.routing.ts
+++ src/app/partial/partial-layout/partial-layout.routing.ts
@@ -1,6 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
 export const PartialLayoutRoutes: Routes = [
+  // asset management
+  { path: 'assets-dashboard', loadChildren: () => import('../asset-management/assets-dashboard/assets-dashboard.module').then(m => m.AssetsDashboardModule), data: { title: 'Assets Dashboard' } },
+
+
+  
   { path: 'dashboard', loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardModule), data: { title: 'Dashboard' } },
-  { path: 'assets-dashboard', loadChildren: () => import('../../partial/assets-dashboard/assets-dashboard.module').then(m => m.AssetsDashboardModule), data: { title: 'Assets Dashboard' } },
 ];
