65e7b5c443b9fe903aaa7fb8ca3f7989d96789d4
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-10-14 14:29:10 +0530

    partial component redesigned according to XD

--- src/app/partial/partial-layout/partial-layout.routing.ts
+++ src/app/partial/partial-layout/partial-layout.routing.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const PartialLayoutRoutes: Routes = [
   // asset management
-  { path: 'assets-dashboard', loadChildren: () => import('../asset-management/assets-dashboard/assets-dashboard.module').then(m => m.AssetsDashboardModule), data: { title: 'Assets Dashboard' } },
+  { path: 'assets-dashboard', loadChildren: () => import('../asset-management/assets-dashboard/assets-dashboard.module').then(m => m.AssetsDashboardModule), data: { title: 'Dashboard' } },
 
 
   
