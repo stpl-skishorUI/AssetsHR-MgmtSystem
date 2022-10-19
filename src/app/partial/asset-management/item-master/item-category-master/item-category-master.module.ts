dab84800df1b247e34b3df1864755b62c83c2288
Sanket <panchmind1@gmail.com>
2022-10-19 14:00:17 +0530

    item category master page design done.

--- src/app/partial/asset-management/item-master/item-category-master/item-category-master.module.ts
+++ src/app/partial/asset-management/item-master/item-category-master/item-category-master.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ItemCategoryMasterRoutingModule } from './item-category-master-routing.module';
 import { ItemCategoryMasterComponent } from './item-category-master.component';
-
-
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 @NgModule({
   declarations: [
     ItemCategoryMasterComponent
   ],
   imports: [
     CommonModule,
-    ItemCategoryMasterRoutingModule
+    ItemCategoryMasterRoutingModule,
+    MaterialModule
   ]
 })
 export class ItemCategoryMasterModule { }
