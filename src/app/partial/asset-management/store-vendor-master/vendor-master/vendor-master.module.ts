8e4c16651457c9bd27bb827574b2aa24d552b87c
navinUI-stpl <navin.boddhul@shauryatechnosoft.com>
2022-10-19 13:58:58 +0530

    Vendor page Designed

--- src/app/partial/asset-management/store-vendor-master/vendor-master/vendor-master.module.ts
+++ src/app/partial/asset-management/store-vendor-master/vendor-master/vendor-master.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MaterialModule } from 'src/app/shared/angularMaterialModule/material.module';
 import { VendorMasterRoutingModule } from './vendor-master-routing.module';
 import { VendorMasterComponent } from './vendor-master.component';
 
@@ -11,7 +11,8 @@ import { VendorMasterComponent } from './vendor-master.component';
   ],
   imports: [
     CommonModule,
-    VendorMasterRoutingModule
+    VendorMasterRoutingModule,
+    MaterialModule
   ]
 })
 export class VendorMasterModule { }
